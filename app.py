2f75de3ac69ca1000bde415c9cdd09d592cdde71
Ana Cristina Carmona Morales <cristy.carmona@gmail.com>
2020-09-30 11:19:21 -0700

    heroku deployment: initial check in (#1)

---
+++ app.py
@@ -0,0 +1,87 @@
+#################################################
+#                      Imports                  #
+#################################################
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+#################################################
+# Set Env to prod when pushing changes to Master 
+# branch, for local environment set to dev.
+# Note: Make sure to update the 
+#################################################
+ENV = 'dev'
+
+if ENV == 'dev':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+#################################################
+#                    Classes                    #
+#################################################
+class Feedback(db.Model):
+    __tablename__ = 'feedback'
+    id = db.Column(db.Integer, primary_key=True)
+    customer = db.Column(db.String(200), unique=True)
+    dealer = db.Column(db.String(200))
+    rating = db.Column(db.Integer)
+    comments = db.Column(db.Text())
+
+    def __init__(self, customer, dealer, rating, comments):
+        self.customer = customer
+        self.dealer = dealer
+        self.rating = rating
+        self.comments = comments
+
+#################################################
+#                   Endpoints                   #
+#################################################
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/areas', methods=['GET'])
+def getMetropolitanAreas():
+    # retrieve data from DB
+
+    # return the json instead of rendering the page
+    return render_template('index.html')
+
+@app.route('/cities', methods=['GET'])
+def getCities():
+    # retrieve data from DB
+
+    # return the json instead of rendering the page
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        # replace submit functionality
+        customer = request.form['customer']
+        dealer = request.form['dealer']
+        rating = request.form['rating']
+        comments = request.form['comments']
+        
+        if customer == '' or dealer == '':
+            return render_template('index.html', message='Please enter required fields')
+        if db.session.query(Feedback).filter(Feedback.customer == customer).count() == 0:
+            data = Feedback(customer, dealer, rating, comments)
+            db.session.add(data)
+            db.session.commit()
+            return render_template('success.html')
+        return render_template('index.html', message='You have already submitted feedback')
+
+#################################################
+#                    Run App                    #
+#################################################
+if __name__ == '__main__':
+    app.run()
